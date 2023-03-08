e856bf940dc0736dd52b85a8ec290389f2b4d53c
JamesHuish <jameshuish1@btinternet.com>
2023-03-08 16:57:20 +0000

    updated image display to show full size image rather than thumbnail

--- app/src/main/java/com/example/agepredict/MainActivity.java
+++ app/src/main/java/com/example/agepredict/MainActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +20,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import com.example.agepredict.ml.AgeModel1;
 import com.example.agepredict.ml.AgeModelcnn;
@@ -27,8 +31,11 @@ import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     TextView result, time;
     ImageView imageView;
     Bitmap bitmap;
-
+    String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,33 +71,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        camaraBtn.setOnClickListener(new View.OnClickListener() {
+        /*camaraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, 12);
             }
-        });
+        });*/
 
+        camaraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Ensure that there's a camera activity to handle the intent
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                "com.example.agepredict.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, 12);
+                    }
+                }
+            }
+        });
         predictBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 try {
-                    AgeModel1 model = AgeModel1.newInstance(MainActivity.this);
+                    AgeModelcnn model = AgeModelcnn.newInstance(MainActivity.this);
                     long start = System.currentTimeMillis();
                     // Creates inputs for reference.
                     TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 200, 200, 3}, DataType.FLOAT32);
                     TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
                     tensorImage.load(bitmap);
                     tensorImage = processor.process(tensorImage);
-                    //bitmap = Bitmap.createScaledBitmap(bitmap,400,400,true);
-                    //TensorImage tensorImage = TensorImage.fromBitmap(bitmap);
-                    TensorBuffer tensorBuffer = tensorImage.getTensorBuffer();
-                    Log.d("tensorImage shape", Arrays.toString(tensorBuffer.getShape()));
-                    Log.d("tensorImage data", Arrays.toString(tensorBuffer.getFloatArray()));
+
                     inputFeature0.loadBuffer(tensorImage.getBuffer());
-                    //inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
 
                     Log.d("inputFeature0 shape", Arrays.toString(inputFeature0.getShape()));
                     Log.d("inputFeature0 data", Arrays.toString(inputFeature0.getFloatArray()));
@@ -105,12 +131,14 @@ public class MainActivity extends AppCompatActivity {
                     inputFeature0.loadArray(inputArray);
                     Log.d("inputFeature0 processed data", Arrays.toString(inputFeature0.getFloatArray()));
                     // Runs model inference and gets result.
-                    AgeModel1.Outputs outputs = model.process(inputFeature0);
+                    AgeModelcnn.Outputs outputs = model.process(inputFeature0);
 
                     TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
                     long inferenceTime = System.currentTimeMillis() - start;
                     Log.d("outputs shape", Arrays.toString(outputFeature0.getShape()));
-                    Log.d("outputs data", Arrays.toString(outputFeature0.getFloatArray()));
+                    Log.d("outputs data", Arrays.toString(outputFeature0.getIntArray()));
+
                     result.setText(outputFeature0.getFloatArray()[0]+"");
                     time.setText(inferenceTime + "");
                     // Releases model resources if no longer used.
@@ -121,6 +149,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     void getPermission(){
@@ -137,26 +181,33 @@ public class MainActivity extends AppCompatActivity {
                     this.getPermission();
                 }
             }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
 
-}
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode==10){
             if(data!=null){
                 Uri uri = data.getData();
+
                 try {
                     bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-                    imageView.setImageBitmap(bitmap);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+                imageView.setImageBitmap(bitmap);
+
             }
         }
         else if(requestCode==12){
-            bitmap = (Bitmap) data.getExtras().get("data");
+            /*bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);*/
+            bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
             imageView.setImageBitmap(bitmap);
         }
         super.onActivityResult(requestCode, resultCode, data);
